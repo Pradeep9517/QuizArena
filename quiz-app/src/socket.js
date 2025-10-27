70bce7cc14eac79863892723cee7cb9e2d3422be
Pradeep <49848640+Pradeep9517@users.noreply.github.com>
2025-10-27 14:15:27 +0530

    Update socket connection URL to production server

--- quiz-app/src/socket.js
+++ quiz-app/src/socket.js
@@ -1,4 +1,4 @@
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:5000"); // Make sure port matches backend
+const socket = io("https://quizarena-8un2.onrender.com"); // Make sure port matches backend
 export default socket;
