521b6488378d69f12f90c8961dca0203abe4567d
Pradeep <49848640+Pradeep9517@users.noreply.github.com>
2025-10-27 14:37:55 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173", // frontend
+    origin: "https://quizarenas.vercel.app", // frontend
     methods: ["GET", "POST"],
   },
 });
